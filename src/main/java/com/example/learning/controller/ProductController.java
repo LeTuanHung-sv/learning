2513c52c660b6819f93e74c40f11368692c195fc
LeTuanHung-sv <letuanhung406@gmail.com>
2026-04-20 11:10:57 +0700

    commit feature delete product

--- src/main/java/com/example/learning/controller/ProductController.java
+++ src/main/java/com/example/learning/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.learning.controller;
 
 import java.util.List;
+import java.util.UUID;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.learning.dto.request.ProductRequestDTO;
 import com.example.learning.dto.response.ProductResponseDTO;
@@ -8,6 +10,7 @@ import com.example.learning.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -29,5 +32,11 @@ public class ProductController {
   @PostMapping("/create")
   public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO dto){
     return ResponseEntity.ok(productService.createProduct(dto));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteProduct(@PathVariable UUID id){
+    productService.deleteProducts(id);
+    return ResponseEntity.ok("Delete successfully");
   }
 }
