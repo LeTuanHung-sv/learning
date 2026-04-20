9350a571ee5fe499e74a76344dff9c910e0fa9d5
LeTuanHung-sv <letuanhung406@gmail.com>
2026-04-20 10:45:50 +0700

    commit updateProducts

--- src/main/java/com/example/learning/controller/ProductController.java
+++ src/main/java/com/example/learning/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.learning.controller;
 
 import java.util.List;
+import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.learning.dto.request.ProductRequestDTO;
 import com.example.learning.dto.response.ProductResponseDTO;
@@ -8,7 +9,9 @@ import com.example.learning.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +32,11 @@ public class ProductController {
   @PostMapping("/create")
   public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO dto){
     return ResponseEntity.ok(productService.createProduct(dto));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<String> updateProduct(@PathVariable UUID id, @Valid @RequestBody ProductRequestDTO dto){
+    productService.updateProducts(id,dto);
+    return ResponseEntity.ok("Products success");
   }
 }
--- src/main/java/com/example/learning/service/ProductService.java
+++ src/main/java/com/example/learning/service/ProductService.java
@@ -3,9 +3,10 @@ package com.example.learning.service;
 import com.example.learning.dto.request.ProductRequestDTO;
 import com.example.learning.dto.response.ProductResponseDTO;
 import java.util.List;
+import java.util.UUID;
 
 public interface ProductService {
   public List<ProductResponseDTO> getProduct ();
   public ProductResponseDTO createProduct(ProductRequestDTO dto);
-
+  public void updateProducts(UUID id, ProductRequestDTO dto);
 }
